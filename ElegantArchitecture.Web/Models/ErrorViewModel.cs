0f7e84dfedadce0a762ccd6e88c87e8c0f534c5f
kinshines <1176807665@qq.com>
2018-07-04 19:13:01 +0800

    first commit

---
+++ ElegantArchitecture.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ElegantArchitecture.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
