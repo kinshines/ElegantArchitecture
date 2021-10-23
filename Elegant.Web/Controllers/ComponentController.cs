3ca33dd2506320245b8cc16d9117ccda592c5350
kinshines <1176807665@qq.com>
2021-10-23 15:11:37 +0800

    typehead

---
+++ Elegant.Web/Controllers/ComponentController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Elegant.Web.Controllers
+{
+    public class ComponentController : Controller
+    {
+        public IActionResult DateTimePicker()
+        {
+            return View();
+        }
+        public IActionResult ColorPicker()
+        {
+            return View();
+        }
+        public IActionResult Select2()
+        {
+            return View();
+        }
+        public IActionResult BootstrapSelect()
+        {
+            return View();
+        }
+
+        public IActionResult MultiSelect()
+        {
+            return View();
+        }
+        public IActionResult MultiSelectDropdown()
+        {
+            return View();
+        }
+
+        public IActionResult SelectSplitter()
+        {
+            return View();
+        }
+        public IActionResult Clipboard()
+        {
+            return View();
+        }
+        public IActionResult Typeahead()
+        {
+            return View();
+        }
+    }
+}
