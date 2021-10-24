292b64b79a6d187f15c580f15bce8ec91efea995
kinshines <1176807665@qq.com>
2021-10-24 11:17:17 +0800

    icheck

---
+++ Elegant.Web/Controllers/FormController.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Elegant.Web.Controllers
+{
+    public class FormController : Controller
+    {
+        public IActionResult Control()
+        {
+            return View();
+        }
+        public IActionResult MaterialControl()
+        {
+            return View();
+        }
+        public IActionResult Validation()
+        {
+            return View();
+        }
+        public IActionResult MaterialValidationState()
+        {
+            return View();
+        }
+        public IActionResult MaterialValidation()
+        {
+            return View();
+        }
+        public IActionResult Layout()
+        {
+            return View();
+        }
+        public IActionResult Repeater()
+        {
+            return View();
+        }
+        public IActionResult InputMask()
+        {
+            return View();
+        }
+        public IActionResult Xeditable()
+        {
+            return View();
+        }
+        public IActionResult Wizard()
+        {
+            return View();
+        }
+        public IActionResult ICheck()
+        {
+            return View();
+        }
+    }
+}
