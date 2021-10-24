7d66d6c16d72918414efe5a56041d5e872cc3e2b
kinshines <1176807665@qq.com>
2021-10-24 22:59:14 +0800

    chart

---
+++ Elegant.Web/Controllers/ChartController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Elegant.Web.Controllers
+{
+    public class ChartController : Controller
+    {
+        public IActionResult Amchart()
+        {
+            return View();
+        }
+        public IActionResult Flotchart()
+        {
+            return View();
+        }
+        public IActionResult Flowchart()
+        {
+            return View();
+        }
+        public IActionResult Google()
+        {
+            return View();
+        }
+        public IActionResult eChart()
+        {
+            return View();
+        }
+        public IActionResult Morris()
+        {
+            return View();
+        }
+        public IActionResult HighChart()
+        {
+            return View();
+        }
+        public IActionResult HighStock()
+        {
+            return View();
+        }
+        public IActionResult HighMap()
+        {
+            return View();
+        }
+    }
+}
