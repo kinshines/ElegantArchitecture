75620b7fc72ff074d6e6311b9d652995d81bfd7e
kinshines <1176807665@qq.com>
2021-10-24 16:21:23 +0800

    table

---
+++ Elegant.Web/Controllers/TableController.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Elegant.Web.Controllers
+{
+    public class TableController : Controller
+    {
+        public IActionResult Basic()
+        {
+            return View();
+        }
+        public IActionResult Responsive()
+        {
+            return View();
+        }
+        public IActionResult Bootstrap()
+        {
+            return View();
+        }
+        public IActionResult DatatablesManaged()
+        {
+            return View();
+        }
+        public IActionResult DatatablesButton()
+        {
+            return View();
+        }
+        public IActionResult DatatablesColreorder()
+        {
+            return View();
+        }
+        public IActionResult DatatablesRowreorder()
+        {
+            return View();
+        }
+        public IActionResult DatatablesScroller()
+        {
+            return View();
+        }
+        public IActionResult DatatablesFixedHeader()
+        {
+            return View();
+        }
+        public IActionResult DatatablesResponsive()
+        {
+            return View();
+        }
+        public IActionResult DatatablesEditable()
+        {
+            return View();
+        }
+        public IActionResult DatatablesAjax()
+        {
+            return View();
+        }
+    }
+}
