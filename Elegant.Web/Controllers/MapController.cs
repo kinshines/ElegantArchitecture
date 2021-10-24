d5ec52e622b7a32664bf6494722d180146225415
kinshines <1176807665@qq.com>
2021-10-24 23:11:25 +0800

    map

---
+++ Elegant.Web/Controllers/MapController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Elegant.Web.Controllers
+{
+    public class MapController : Controller
+    {
+        public IActionResult Google()
+        {
+            return View();
+        }
+        public IActionResult Vector()
+        {
+            return View();
+        }
+    }
+}
