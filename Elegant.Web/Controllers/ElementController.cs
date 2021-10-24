8eaef36a0e073deee4990ec69c76649ae46c0539
kinshines <1176807665@qq.com>
2021-10-24 15:04:47 +0800

    element

---
+++ Elegant.Web/Controllers/ElementController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Elegant.Web.Controllers
+{
+    public class ElementController : Controller
+    {
+        public IActionResult Step()
+        {
+            return View();
+        }
+        public IActionResult List()
+        {
+            return View();
+        }
+        public IActionResult Ribbon()
+        {
+            return View();
+        }
+        public IActionResult Overlay()
+        {
+            return View();
+        }
+        public IActionResult UserCard()
+        {
+            return View();
+        }
+    }
+}
