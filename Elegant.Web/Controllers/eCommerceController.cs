815863e71794480c10e08f5c22cab00a22cf7ca3
kinshines <1176807665@qq.com>
2021-10-25 22:47:34 +0800

    eCommercen Dashboard

---
+++ Elegant.Web/Controllers/eCommerceController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Elegant.Web.Controllers
+{
+    public class eCommerceController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Order()
+        {
+            return View();
+        }
+        public IActionResult OrderView()
+        {
+            return View();
+        }
+        public IActionResult Product()
+        {
+            return View();
+        }
+        public IActionResult ProductEdit()
+        {
+            return View();
+        }
+    }
+}
