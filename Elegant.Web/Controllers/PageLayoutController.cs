a240b023769d04091ef955034ccc978a68b33214
kinshines <1176807665@qq.com>
2021-10-25 21:49:34 +0800

    page layout

---
+++ Elegant.Web/Controllers/PageLayoutController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Elegant.Web.Controllers
+{
+    public class PageLayoutController : Controller
+    {
+        public IActionResult Blank()
+        {
+            return View();
+        }
+        public IActionResult LanguageBar()
+        {
+            return View();
+        }
+        public IActionResult FixedFooter()
+        {
+            return View();
+        }
+        public IActionResult Boxed()
+        {
+            return View();
+        }
+
+        public IActionResult LanguageOption()
+        {
+            return PartialView();
+        }
+    }
+}
