3988fe099090206a4a2fb81f9f41a957b0f0685b
kinshines <1176807665@qq.com>
2021-10-25 22:15:31 +0800

    SidebarLayout

---
+++ Elegant.Web/Controllers/SidebarLayoutController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Elegant.Web.Controllers
+{
+    public class SidebarLayoutController : Controller
+    {
+        public IActionResult Hover()
+        {
+            return View();
+        }
+        public IActionResult Reversed()
+        {
+            return View();
+        }
+        public IActionResult Fixed()
+        {
+            return View();
+        }
+        public IActionResult Closed()
+        {
+            return View();
+        }
+    }
+}
