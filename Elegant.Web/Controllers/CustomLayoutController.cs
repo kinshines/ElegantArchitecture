8ab192fe3d64842ae4e4315e45b3ea805f6880f5
kinshines <1176807665@qq.com>
2021-10-25 22:34:48 +0800

    custom layout

---
+++ Elegant.Web/Controllers/CustomLayoutController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Elegant.Web.Controllers
+{
+    public class CustomLayoutController : Controller
+    {
+        public IActionResult DisabledMenu()
+        {
+            return View();
+        }
+        public IActionResult DisabledLink()
+        {
+            return PartialView();
+        }
+    }
+}
