1607a82b443d180bf4565f5933df6175bde4c97a
kinshines <1176807665@qq.com>
2021-10-26 22:56:04 +0800

    app

---
+++ Elegant.Web/Controllers/AppController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Elegant.Web.Controllers
+{
+    public class AppController : Controller
+    {
+        public IActionResult Todo()
+        {
+            return View();
+        }
+
+        public IActionResult Todo2()
+        {
+            return View();
+        }
+
+        public IActionResult Ticket()
+        {
+            return View();
+        }
+
+        public IActionResult Inbox()
+        {
+            return View();
+        }
+
+        public IActionResult InboxMessage()
+        {
+            return PartialView();
+        }
+
+        public IActionResult Calendar()
+        {
+            return View();
+        }
+        public IActionResult Config()
+        {
+            return View();
+        }
+        public IActionResult Staff()
+        {
+            return View();
+        }
+        public IActionResult Detail()
+        {
+            return View();
+        }
+    }
+}
